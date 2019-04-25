458e77b59c59e68e0469fc4b6cf4fca9f7c75c01
Borja <borjabombi@gmail.com>
2019-04-25 14:43:05 +0200

    Packages and plotting

--- Script/Associatons.R
+++ Script/Associatons.R
@@ -1,7 +1,8 @@
 ###############################################################################
 
 # Packages:
-pacman::p_load(rstudioapi, readr, caret, corrplot, e1071, dplyr, car, GGally, arules, arulesViz)
+pacman::p_load(rstudioapi, readr, RColorBrewer, dplyr, car, GGally, 
+               arules, arulesViz)
 
 # Load data ---------------------------
 
@@ -24,4 +25,10 @@ mean(size(transactions))
 LIST(transactions)
 itemLabels(transactions)
 
+# Plotting
+barplot(sort(itemFrequency(transactions), decreasing=TRUE))
+itemFrequencyPlot(transactions, topN = 10, type = "absolute", 
+                  col = brewer.pal(8, "Pastel2"),
+                  main = "Top10 - Absolute Item Frequency Plot" )
 
+image(sample(transactions,500))
