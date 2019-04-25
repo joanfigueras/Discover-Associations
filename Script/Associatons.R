1f98a9fbfb54dae5d38f1dd5142cb49ee0c93fdd
Yago Cordón <yagocordonguerrero@gmail.com>
2019-04-25 14:20:49 +0200

    importing dataset

--- Script/Associatons.R
+++ Script/Associatons.R
@@ -1,7 +1,7 @@
 ###############################################################################
 
 # Packages:
-pacman::p_load(rstudioapi, readr, caret, corrplot, e1071, dplyr, car, GGally)
+pacman::p_load(rstudioapi, readr, caret, corrplot, e1071, dplyr, car, GGally, arules, arulesViz)
 
 # Load data ---------------------------
 
@@ -10,6 +10,16 @@ current_path <- getActiveDocumentContext()$path
 setwd(dirname(dirname(current_path)))
 rm(current_path)
 
+#Data Frame
 transact_2017 <- read.csv("datasets/ElectronidexTransactions2017.csv",
                           header = FALSE)
+#Transactions
+transactions <- read.transactions(file = "Datasets/ElectronidexTransactions2017.csv",
+                                 format = "basket",
+                                 sep = ",",
+                                 header = FALSE,
+                                 cols = 1)
+
+
+
 
