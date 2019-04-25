0fc2f6ce6c4e9ebc8b1e581828c57e822e1848cd
Yago Cordón <yagocordonguerrero@gmail.com>
2019-04-25 17:12:54 +0200

    Set dataset and integer

--- Script/Associatons.R
+++ Script/Associatons.R
@@ -1,7 +1,7 @@
 ###############################################################################
 
 # Packages:
-pacman::p_load(rstudioapi, readr, caret, corrplot, e1071, dplyr, car, GGally)
+pacman::p_load(rstudioapi, readr, caret, corrplot, e1071, dplyr, car, GGally, arules, arulesViz)
 
 # Load data ---------------------------
 
@@ -10,6 +10,18 @@ current_path <- getActiveDocumentContext()$path
 setwd(dirname(dirname(current_path)))
 rm(current_path)
 
-transact_2017 <- read.csv("datasets/ElectronidexTransactions2017.csv",
-                          header = FALSE)
+#Transactions
+transactions <- read.transactions(file = "datasets/ElectronidexTransactions2017.csv",
+                                 format = "basket",
+                                 sep = ",",
+                                 header = FALSE,
+                                 cols = 1)
+
+#Get to know dataset
+inspect(transactions[1:1000])
+length(transactions)
+mean(size(transactions))
+LIST(transactions)
+itemLabels(transactions)
+
 
