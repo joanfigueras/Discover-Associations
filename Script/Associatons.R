672c47c89f39c570d773029838324b61f591a8e0
Borja <borjabombi@gmail.com>
2019-04-29 12:47:09 +0200

    Preprocessing to find type of customer

--- Script/Associatons.R
+++ Script/Associatons.R
@@ -1,7 +1,7 @@
 #Settings ----------------
 # Packages:
 pacman::p_load(rstudioapi, readr, RColorBrewer, dplyr, car, GGally, shiny,
-               arules, arulesViz)
+               arules, arulesViz, ggplot2)
 
 # Load data ---------------------------
 # Datasets:
@@ -21,12 +21,8 @@ transactions@itemInfo$category <- Item_Cat[ ,2]
 str(transactions)
 by_category <- aggregate(transactions, by = "category")
 
-#Get to know dataset
-inspect(transactions[1:1000])
-length(transactions)
-mean(size(transactions))
-LIST(transactions)
-itemLabels(transactions)
+transactions@itemInfo$labels <- paste(transactions@itemInfo$category,
+                                      transactions@itemInfo$labels)
 
 # Matrix and Data Frame from the "transactions":
 transactions_mat <- as(transactions,"matrix")
@@ -36,6 +32,8 @@ for (i in 1:ncol(transactions_df)) {
   transactions_df[ ,i] <- as.integer(transactions_df[ ,i])
 }
 
+transactions_df$client_type <- #Here is where we need to create the new variable for the type of customers
+
 # Plotting -------------------------------------------------------
 barplot(sort(itemFrequency(transactions), decreasing=TRUE))
 itemFrequencyPlot(transactions, topN = 10, type = "absolute", 
@@ -43,19 +41,25 @@ itemFrequencyPlot(transactions, topN = 10, type = "absolute",
                   main = "Top10 - Absolute Item Frequency Plot" )
 
 image(sample(transactions,500))
+image(sample(by_category,100))
 
 itemFrequencyPlot(by_category, topN = 20, type = "absolute", 
                   col = brewer.pal(8, "Pastel2"),
                   main = "Top10 - Absolute Item Frequency Plot" )
 
+itemFrequencyPlot(subset(by_category = "Laptops"), topN = 20, type = "absolute", 
+                  col = brewer.pal(8, "Pastel2"),
+                  main = "Top10 - Absolute Item Frequency Plot" )
+
  # Rules -----------------------------------------------------------------
-association_rules <- apriori (by_category, 
-                              parameter = list(supp = 0.01, conf = 0.9, 
+association_rules <- apriori (transactions, 
+                              parameter = list(supp = 0.0015, conf = 0.9, 
                                                minlen = 2)) #We use minlen to set the minimun size for the baskets
 
 
 redundant_rules <- is.redundant(association_rules)
 association_rules <- association_rules[!redundant_rules] # remove redundant rules.
+
 ruleExplorer(association_rules) # Cool stuff
 inspect(association_rules)
 
@@ -63,13 +67,14 @@ top_confidence <- sort(association_rules, decreasing = TRUE, na.last = NA,
                        by = "confidence")
 inspect(top_confidence)
 a <- c("Laptops", "Desktop")
-rules_iMac <- apriori(by_category, # All the baskets that are more likely to buy an iMac
-                      parameter = list(supp = 0.0005, conf = 0.1), 
+category_rules <- apriori(by_category, # All the baskets that are more likely to buy an iMac
+                      parameter = list(conf = 0.3), 
                       appearance = list(default = "rhs", lhs = a))
 
-inspect(rules_iMac)
+inspect(category_rules)
 
-subset(association_rules, items %in% "iMac")
-plot(subset(association_rules, items %in% "Acer Desktop"), method = "graph", control = list(type = "items"))
+ruleExplorer(category_rules)
 
+# subset(association_rules, items %in% "iMac")
+# plot(subset(association_rules, items %in% "Acer Desktop"), method = "graph", control = list(type = "items"))
 
